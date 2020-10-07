d2754132f77f69e275bff0799198a7233a696a16
Minsi Yang <yangminsi@gmail.com>
2020-10-07 21:09:03 +0100

    Pass test for passing in multiple pairs of brackets in order

--- src/BracketsValidator.js
+++ src/BracketsValidator.js
@@ -5,14 +5,16 @@ function bracketsValidator(string) {
     "{": "}"
   }
 
-  if (string === "" || string.length === 2 &&
-      string[1] === data[string[0]]) {
-      // string[0] === "(" && string[1] === ")" ||
-      // string[0] === "[" && string[1] === "]" ||
-      // string[0] === "{" && string[1] === "}" ) {
-    return true
-  } else {
-    return false
+  if (string === "" ) return true; 
+  for (let i = 0; i < string.length; i += 2) {
+    if (string[i+1] === data[string[i]]) {
+        // string[0] === "(" && string[1] === ")" ||
+        // string[0] === "[" && string[1] === "]" ||
+        // string[0] === "{" && string[1] === "}" ) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 module.exports = bracketsValidator;
