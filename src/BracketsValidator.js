b1191342065e8be69655bc2c4058622d211535cb
Minsi Yang <yangminsi@gmail.com>
2020-10-07 16:41:51 +0100

    Refactor bracketsValidator() for passing in full pairs of brackets

--- src/BracketsValidator.js
+++ src/BracketsValidator.js
@@ -1,8 +1,15 @@
 function bracketsValidator(string) {
+  const data = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  }
+
   if (string === "" || 
-      string[0] === "(" && string[1] === ")" ||
-      string[0] === "[" && string[1] === "]" ||
-      string[0] === "{" && string[1] === "}" ) {
+      string[1] === data[string[0]]) {
+      // string[0] === "(" && string[1] === ")" ||
+      // string[0] === "[" && string[1] === "]" ||
+      // string[0] === "{" && string[1] === "}" ) {
     return true
   } else {
     return false
